47a3f3b52ba57c45125ad6bbafb83dd2b9163418
Leandro-Santos-Santos <santosleandro1805@gmail.com>
2024-03-03 21:15:07 -0300

    Teste

--- banco.py
+++ banco.py
@@ -6,41 +6,40 @@ class Conta:
         self.idade = idade
 
 def criar_conta():
-    nome = input('Qual é o seu nome: ')
+    nome = str (input('Qual é o seu nome: '))
     print('Olá ' + nome + ', bem-vindo ao Banco XPTO!')
-    sexo = input('Qual é o seu gênero: ')
-    idade = int(input('Qual é a sua idade: '))
-    cpf = input('Digite o seu CPF: ')
-    senha = input('Digite a sua nova senha: ')
-    numero_da_conta = 14563066  # Você pode querer gerar um número aleatório
+    sexo = str (input('Qual é o seu gênero: '))
+    idade = int (input('Qual é a sua idade: '))
+    cpf = int (input('Digite o seu CPF: '))
+    senha = int (input('Digite a sua nova senha: '))
+    numero_da_conta = int (input('Digite o numero da conta'))
     return Conta(nome, sexo, cpf, idade)
 
 def acessar_conta(numero_da_conta, senha):
 
+def remover_conta(Conta ):
 
-def remover_conta():
-
-opcao = 0
-while opcao != 4:
+opção = 0
+while opção != 4:
     print('''[1] Criar Conta
 [2] Acessar Conta
 [3] Remover Conta
 [4] Sair''')
-    opcao = int(input('Escolha a opção: '))
+    opção = int(input('Escolha a opção: '))
 
-    if opcao == 1:
+    if opção == 1:
         nova_conta = criar_conta()
         print('Conta criada com sucesso!')
 
-    elif opcao == 2:
+    elif opção == 2:
         numero_conta = input('Digite o número da conta: ')
         senha = input('Digite a sua senha: ')
         acessar_conta(numero_conta, senha)
 
-    elif opcao == 3:
+    elif opção == 3:
         remover_conta()
 
-    elif opcao == 4:
+    elif opção == 4:
         print('Finalizando...')
     else:
         print('Opção inválida. Tente novamente')
