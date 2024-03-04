e126a4727a038ecbadaaf722d61770ab071b089d
Leandro-Santos-Santos <santosleandro1805@gmail.com>
2024-03-04 15:10:59 -0300

    Colocando comentário

--- banco.py
+++ banco.py
@@ -1,3 +1,4 @@
+# Essa é uma classe POO
 class Conta:
     def __init__(self, nome, sexo, cpf, idade, senha, numero_da_conta):
         self.nome = nome
@@ -7,7 +8,7 @@ class Conta:
         self.senha = senha
         self.numero_da_conta = numero_da_conta
 
-
+# Defenir a função criar conta
 def criar_conta():
     nome = str (input('Qual é o seu nome: '))
     print('Olá ' + nome + ', bem-vindo ao Banco XPTO!')
@@ -18,15 +19,18 @@ def criar_conta():
     numero_da_conta = int (input('Digite o numero da conta'))
     return Conta(nome, sexo, cpf, idade, senha, numero_da_conta)
 
+# Defenir a função acessar conta
 def acessar_conta(numero_da_conta, senha):
     numero_conta = int (input('Digite o numero da conta: '))
     senha = int (input('Digite sua senha: '))
     return Conta(numero_da_conta, senha)
 
-
+# Definir a função remover conta
 def remover_conta(numero_da_conta, senha ):
     print(f'Remover Conta')
 
+# Lógica de while com combinação if elif e else
+# Acesso ao Menu Principal    
 opção = 0
 while opção != 4:
     print('''[1] Criar Conta
